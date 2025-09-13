a08d66ea34d9deb906a734e1b44ff7820e6f1585
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-13 20:12:43 +0600

    Exploring Raw Node js

--- 7.2/index.js
+++ 7.2/index.js
@@ -1,13 +1,14 @@
-// Local Module -----------------------
+// Local Module (Common js style) -----------------------
 
 // const add = require("./local-1.js");
 
-const { a, add } = require("./local-1.js");
+const { a, add } = require("./local-1.js"); // we can destructure as well
 
-const { a: a2, add: add2 } = require("./local-2.js");
+const { a: a2, add: add2 } = require("./local-2.js"); // as both params are same, so we use name alias
 
 // console.log(add.add(3, 5));
 // console.log(add(2, 3));
+// console.log(add2(2, 3, 4));
 
 // Build-in Module --------------------
 const path = require("path");

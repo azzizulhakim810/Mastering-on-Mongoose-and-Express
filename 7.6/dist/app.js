69303e35ac6adf1fa7013ec780f8dbec2aa43220
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-14 20:08:46 +0600

    Explored Parser

--- 7.6/dist/app.js
+++ 7.6/dist/app.js
@@ -6,7 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+// Parser
+app.use(express_1.default.json()); // Parse Json
+app.use(express_1.default.text()); // Parse Raw Text
 app.get("/", (req, res) => {
-    res.send("Hello from Bangladesh!");
+    res.send("Hello World from Bangladesh!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    // res.send("Got Data");
+    res.json({
+        message: "Successfully Received Data",
+    });
 });
 exports.default = app;
--- 7.6/src/app.ts
+++ 7.6/src/app.ts
@@ -1,9 +1,21 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from Bangladesh!");
+// Parser
+app.use(express.json()); // Parse Json
+app.use(express.text()); // Parse Raw Text
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World from Bangladesh!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  // res.send("Got Data");
+  res.json({
+    message: "Successfully Received Data",
+  });
 });
 
 export default app;

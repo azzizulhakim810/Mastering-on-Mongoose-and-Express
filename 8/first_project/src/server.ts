62abe53ceace1b4ddfac0d08f8a92e7acb03277c
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-16 12:41:20 +0600

    Complete the basic setup & added two more script for production & developemnt phase

--- 8/first_project/src/server.ts
+++ 8/first_project/src/server.ts
@@ -1,7 +1,6 @@
-import app from "./app";
-import config from "./app/config";
-import mongoose from "mongoose";
-// require("dotenv").config();
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
 
 async function main() {
   try {
@@ -13,8 +12,6 @@ async function main() {
   } catch (error) {
     console.log(error);
   }
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 main();

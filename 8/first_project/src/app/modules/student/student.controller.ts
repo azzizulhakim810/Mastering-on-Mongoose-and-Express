5ff46a33e19f8936ca3b9148a8c4e47a83c84a95
azzizulhakim810 <azzizul.hakim202@gmail.com>
2025-09-16 22:07:43 +0600

    Find all by another query

--- 8/first_project/src/app/modules/student/student.controller.ts
+++ 8/first_project/src/app/modules/student/student.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
 
+// Insert
 const createStudent = async (req: Request, res: Response) => {
   try {
     const student = req.body;
@@ -17,6 +18,22 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+// Retrieve
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved Successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
 };
--- 8/first_project/src/app/modules/student/student.service.ts
+++ 8/first_project/src/app/modules/student/student.service.ts
@@ -1,11 +1,19 @@
 import { Student } from './student.interface';
 import { StudentModel } from './student.model';
 
+// Insert
 const createStudentIntoDB = async (student: Student) => {
   const result = await StudentModel.create(student);
   return result;
 };
 
+// Retrieve
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
 export const StudentServices = {
   createStudentIntoDB,
+  getAllStudentsFromDB,
 };
